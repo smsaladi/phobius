0cd2beede0a2bd6f4476b81bf0424c1e78bf3c24
SuperMario210 <Sup3rMar1o210@gmail.com>
2019-11-05 22:05:22 -0800

    updated html wrapper to have a split console with text sequence input and args input

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const output = document.getElementById('console');
 // holds the current instance of the Phobius module.  Used to load files
 var Module;
+const default_args = ["preload/phobius.options", "preload/phobius.model", null]
 
 function clearConsole() {
     output.innerHTML = "";
@@ -58,6 +59,20 @@ function download(filename, data) {
     }
 }
 
+function updateFileLabel(element) {
+    const file = element.files[0]
+    const label = document.getElementById(element.id + "-label");
+    const selector = document.getElementById(element.id + "-selector");
+
+    if(file) {
+        selector.classList.add("green");
+        label.innerHTML = "&nbsp;(" + file.name + ")"
+    } else {
+        selector.classList.remove("green");
+        label.innerHTML = "&nbsp;(default)"
+    }
+}
+
 //
 //  Reads file data from the input_element and base 64 encodes it
 //
@@ -86,37 +101,45 @@ async function getFileData(input_element) {
 //  Synchronously reads file data from all input elements on the page
 //
 async function getFiles() {
-    inputs = document.getElementsByTagName('input');
+    inputs = document.querySelectorAll("input[type=file]");
     files = [];
     for (input of inputs) {
         files.push(await getFileData(input));
     }
+
+    if(!files[files.length - 1]) {
+        const sequence = document.getElementById("sequence-input").value;
+        files[files.length - 1] = {name: "seq.txt", data: btoa(sequence)};
+    }
+
     return files;
 }
 
 //
 //  Initialize the module parameters given the input files
 //
-function initModuleParams(files) {
+function initModuleParams(files, extra_args) {
     // Build the program arguments
     let args = ["-f"];
+    let i = 0;
     for(const file of files) {
         // Fail if we are missing a file
-        if(!file) {
+        if(!file && !default_args[i]) {
             return false;
         }
 
-        args.push(file.name);
+        args.push(file ? file.name : default_args[i]);
+        i++;
     }
 
     return {
-        arguments: args,
+        arguments: args.concat(extra_args),
         preRun: [function() {
             // Mount the files to the Module
             let mounted_files = {};
             for(const file of files) {
                 // Don't load the same file twice
-                if(mounted_files[file.name]) {
+                if(!file || mounted_files[file.name]) {
                     continue;
                 }
 
@@ -147,18 +170,25 @@ function initModuleParams(files) {
     }
 }
 
+function getExtraArgs() {
+    const raw = document.getElementById("args").value;
+    const formatted = raw.replace(/\s\s+/g, ' ').trim();
+    return formatted.length > 0 ? formatted.split(" ") : [];
+}
+
 //
 //  Sets up and runs the Phobius module
 //
 async function runPhobius() {
     files = await getFiles();
+    extra_args = getExtraArgs();
 
-    const module_parameters = initModuleParams(files)
+    const module_parameters = initModuleParams(files, extra_args)
     if(module_parameters) {
         clearConsole();
         logConsole("Loading...", false);
         Module = Phobius(module_parameters);
     } else {
-        logConsole("You must provide a model, options, and sequence file!", true);
+        logConsole("You must provide an input sequence!", true);
     }
 }
